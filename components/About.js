15927b46a57ecb73aa288d1d2971b37037324b87
mparente88 <parente.molly@gmail.com>
2024-12-18 16:43:39 -0800

    changed img tag to Image for better performance

--- components/About.js
+++ components/About.js
@@ -1,8 +1,10 @@
+import Image from "next/image"
+
 export default function About() {
   return (
     <section id="about" className="about section">
       <div className="about-container">
-        <img src="/profile-placeholder.png" alt="Profile" className="profile-img" />
+        <Image src="/profile-placeholder.png" alt="Profile" width={150} height={150} className="profile-img" />
         <p>Hi! I’m Molly, a software engineer with a background in teaching and a passion for solving complex problems...</p>
       </div>
     </section>
