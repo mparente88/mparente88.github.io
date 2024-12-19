c0b4d97cadfee6ea262387e88a2990e5cf20725f
mparente88 <parente.molly@gmail.com>
2024-12-18 16:37:59 -0800

    updated Hero banner name

--- components/Hero.js
+++ components/Hero.js
@@ -1,7 +1,7 @@
 export default function Hero() {
   return (
     <section className="hero section">
-      <h1>Hi, I&apos;m Molly</h1>
+      <h1>Molly Parente</h1>
       <p>A Software Engineer passionate about creating efficient, user-centric applications.</p>
       <div className="hero-buttons">
         <button>View Projects</button>
