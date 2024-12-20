fa52ec3bece00ae938c8e913290b3e0e42a8b873
mparente88 <parente.molly@gmail.com>
2024-12-20 11:22:43 -0800

    Added tooltips to the navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,20 +1,41 @@
 import Link from "next/link"
+import Image from "next/image"
 
 export default function Navbar() {
   return (
     <nav className="nav">
       <ul className="nav-list">
         <li>
-          <Link href="/">Home</Link>
+          <Link href="/">
+            <div className="nav-item">
+              <Image src="/new-house.gif" alt="Home" unoptimized width={50} height={50} />
+              <span className="tooltip">Home</span>
+            </div>
+          </Link>
         </li>
         <li>
-          <Link href="/about">About</Link>
+          <Link href="/about">
+            <div className="nav-item">
+              <Image src="/info.gif" alt="About" unoptimized width={50} height={50} />
+              <span className="tooltip">About</span>
+            </div>
+          </Link>
         </li>
         <li>
-          <Link href="/projects">Projects</Link>
+          <Link href="/projects">
+            <div className="nav-item">
+              <Image src="/portfolio.gif" alt="Projects" unoptimized width={50} height={50} />
+              <span className="tooltip">Projects</span>
+            </div>
+          </Link>
         </li>
         <li>
-          <Link href="/contact">Contact</Link>
+          <Link href="/contact">
+            <div className="nav-item">
+              <Image src="/contact.gif" alt="Contact" unoptimized width={50} height={50} />
+              <span className="tooltip">Contact</span>
+            </div>
+          </Link>
         </li>
       </ul>
     </nav>
