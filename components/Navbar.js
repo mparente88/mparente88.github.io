4ca965d5b21e63b75483ee99faf3471b207580e2
mparente88 <parente.molly@gmail.com>
2024-12-21 11:28:35 -0800

    Prefetch navbar links

--- components/Navbar.js
+++ components/Navbar.js
@@ -23,7 +23,7 @@ export default function Navbar() {
     <nav className="nav">
       <ul className="nav-list">
         <li>
-          <Link href="/" aria-label="Home">
+          <Link href="/" prefetch aria-label="Home">
             <div className={`nav-item ${router.pathname === "/" ? "active" : ""}`}>
               <Image src="/new-house.gif" alt="Home" unoptimized width={50} height={50} />
               <span className="tooltip">Home</span>
@@ -31,7 +31,7 @@ export default function Navbar() {
           </Link>
         </li>
         <li>
-          <Link href="/about" aria-label="About Me">
+          <Link href="/about" prefetch aria-label="About Me">
             <div className={`nav-item ${router.pathname === "/about" ? "active" : ""}`}>
               <Image src="/info.gif" alt="About" unoptimized width={50} height={50} />
               <span className="tooltip">About</span>
@@ -39,7 +39,7 @@ export default function Navbar() {
           </Link>
         </li>
         <li>
-          <Link href="/projects" aria-label="Projects">
+          <Link href="/projects" prefetch aria-label="Projects">
             <div className={`nav-item ${router.pathname === "/projects" ? "active" : ""}`}>
               <Image src="/portfolio.gif" alt="Projects" unoptimized width={50} height={50} />
               <span className="tooltip">Projects</span>
@@ -47,7 +47,7 @@ export default function Navbar() {
           </Link>
         </li>
         <li>
-          <Link href="/endorsements" aria-label="Endorsements">
+          <Link href="/endorsements" prefetch aria-label="Endorsements">
             <div className={`nav-item ${router.pathname === "/endorsements" ? "active" : ""}`}>
               <Image src="/friend.gif" alt="Endorsements" unoptimized width={50} height={50} />
               <span className="tooltip">Endorsements</span>
@@ -55,7 +55,7 @@ export default function Navbar() {
           </Link>
         </li>
         <li>
-          <Link href="/contact" aria-label="Contact Me">
+          <Link href="/contact" prefetch aria-label="Contact Me">
             <div className={`nav-item ${router.pathname === "/contact" ? "active" : ""}`}>
               <Image src="/contact.gif" alt="Contact" unoptimized width={50} height={50} />
               <span className="tooltip">Contact</span>
