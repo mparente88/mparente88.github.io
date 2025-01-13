b8fe90dedf211bd201c2f61c854997c798b4a7e2
mparente88 <parente.molly@gmail.com>
2025-01-13 13:30:17 -0800

    update Hero.js with profile picture and Typed subtext

--- components/Hero.js
+++ components/Hero.js
@@ -1,17 +1,43 @@
-import { useRouter } from "next/router"
+import React, { useEffect, useRef } from "react"
+import Typed from "typed.js"
+import Image from "next/image"
 
 export default function Hero() {
-  const router = useRouter()
+  const typedRef = useRef(null)
+
+  useEffect(() => {
+    const options = {
+      strings: ["Software Engineer", "Problem Solver", "Creative Thinker"],
+      typeSpeed: 50,
+      backSpeed: 30,
+      loop: true,
+    }
+    const typed = new Typed(typedRef.current, options)
+
+    return () => {
+      typed.destroy()
+    }
+  }, [])
 
   return (
     <section className="hero">
+      {/* Profile Picture*/}
+      <div className="profile-picture">
+        <Image src="/profilePicture.png" alt="Molly's Profile Picture" width={150} height={150} className="profile-img" />
+      </div>
+
+      {/* Hero Text */}
       <h1>Hi, I&apos;m Molly</h1>
-      <p>A Software Engineer passionate about creating efficient, user-centric applications.</p>
-      <span>
-        <a href="/MollyParenteResume.pdf" download className="btn">
-          Download My Resume
-        </a>
-      </span>
+      <p className="typed-container">
+        <span ref={typedRef}></span>
+      </p>
+      <div>
+        <button>
+          <a href="/MollyParenteResume.pdf" download className="btn">
+            Download My Resume
+          </a>
+        </button>
+      </div>
       <div>
         <button onClick={() => router.push("/projects")}>View Projects</button>
         <button onClick={() => router.push("/contact")}>Contact Me</button>
