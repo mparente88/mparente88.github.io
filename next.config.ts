cdfb5cdc2d4f517ed264cbd2819b175a3fa42ad4
mparente88 <parente.molly@gmail.com>
2025-01-14 10:15:59 -0800

    prep for deployment

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
   assetPrefix: process.env.NODE_ENV === "production" ? "/mparente88.github.io/" : undefined,
+  images: {
+    unoptimized: true,
+  },
 }
 
 export default nextConfig
