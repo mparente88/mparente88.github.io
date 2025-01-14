30c86f1206cd8776c6bd63446c41ac6132d8aba7
mparente88 <parente.molly@gmail.com>
2025-01-14 12:46:56 -0800

    fix CSS path issues

--- components/Hero.js
+++ components/Hero.js
@@ -1,10 +1,8 @@
 import React, { useEffect, useRef } from "react"
 import Typed from "typed.js"
 import Image from "next/image"
-import { useRouter } from "next/router"
 
 export default function Hero() {
-  const router = useRouter()
   const typedRef = useRef(null)
 
   useEffect(() => {
@@ -24,7 +22,7 @@ export default function Hero() {
   return (
     <section className="hero">
       <div className="profile-picture">
-        <Image src={`${router.basePath}/profilePicture.png`} alt="Molly's Profile Picture" width={150} height={150} className="profile-img" />
+        <Image src="/profilePicture.png" alt="Molly's Profile Picture" width={150} height={150} className="profile-img" />
       </div>
 
       <h1>Hi, I&apos;m Molly</h1>
@@ -39,8 +37,8 @@ export default function Hero() {
         </button>
       </div>
       <div>
-        <button onClick={() => router.push(`${router.basePath}/projects`)}>View Projects</button>
-        <button onClick={() => router.push(`${router.basePath}/contact`)}>Contact Me</button>
+        <button onClick={() => router.push("/projects")}>View Projects</button>
+        <button onClick={() => router.push("/contact")}>Contact Me</button>
       </div>
     </section>
   )
