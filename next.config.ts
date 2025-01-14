ef763ffbed93c9b2c0c0b2404a3f11f5033e3f19
mparente88 <parente.molly@gmail.com>
2025-01-14 10:45:10 -0800

    removed mistake from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from "next"
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
-  basePath: "/mparente88.github.io",
-  assetPrefix: process.env.NODE_ENV === "production" ? "/mparente88.github.io/" : undefined,
+  // basePath: "/mparente88.github.io",
+  assetPrefix: process.env.NODE_ENV === "production" ? "/mparente88.github.io" : undefined,
   images: {
     unoptimized: true,
   },
