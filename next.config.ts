3c446cef47142800149e3612554ce78d64e2f635
mparente88 <parente.molly@gmail.com>
2025-01-14 14:39:15 -0800

    fix project card image overlap

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
   basePath: "/mparente88.github.io",
-  assetPrefix: "/mparente88.github.io",
+  assetPrefix: "https://mparente88.github.io",
   images: {
     unoptimized: true,
   },
