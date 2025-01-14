87b5d7910518bed5d172b3bab7e734ce421ed0e8
mparente88 <parente.molly@gmail.com>
2025-01-14 12:18:26 -0800

    fix CSS path issues

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
 import type { NextConfig } from "next"
 
-const isProd = process.env.NODE_ENV === "production"
+// const isProd = process.env.NODE_ENV === "production"
 
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
-  assetPrefix: "/mparente88.github.io",
+  assetPrefix: "https://mparente88.github.io",
   basePath: "/mparente88.github.io",
   images: {
     unoptimized: true,
