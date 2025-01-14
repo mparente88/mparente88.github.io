8dcf7ab4e1062f064125e2675b9a58abd3b22f83
mparente88 <parente.molly@gmail.com>
2025-01-14 10:21:40 -0800

    new github actions to deploy page

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next"
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
+  basePath: "/mparente88.github.io",
   assetPrefix: process.env.NODE_ENV === "production" ? "/mparente88.github.io/" : undefined,
   images: {
     unoptimized: true,
