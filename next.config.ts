6434d0bf446b95528ac855bebe3eaea491a3b813
mparente88 <parente.molly@gmail.com>
2025-01-14 11:08:47 -0800

    include correct css path

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next"
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
-  // basePath: "/mparente88.github.io",
   assetPrefix: process.env.NODE_ENV === "production" ? "/mparente88.github.io" : undefined,
   images: {
     unoptimized: true,
